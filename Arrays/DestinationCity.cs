755d89e84792a14b9575025a60f57f00ef70f5f3
Sarath Uthirasamy <sarath.u1398@gmail.com>
2025-08-24 01:25:28 +0530

    Added Leetcode Solution for #1436 Destination City

---
+++ Arrays/DestinationCity.cs
@@ -0,0 +1,25 @@
+namespace Arrays;
+
+public class DestinationCity : Solution
+{
+    public string DestCity(IList<IList<string>> paths) {
+        // Add all source and destination paths in a dictionary
+        Dictionary<string,string> pathMap = new();
+        foreach (var path in paths)
+        {
+            pathMap.Add(path[0], path[1]);
+        }
+        // Try to check if one destination is the source for any other path.
+        // If not, then that's the final destination
+        foreach(var path in paths)
+        {
+            string dest = pathMap[path[0]];
+            bool isAvailable = pathMap.TryGetValue(dest, out string new_dest);
+            if(!isAvailable)
+            {
+                return dest;
+            }
+        }
+        return String.Empty;
+    }
+}
