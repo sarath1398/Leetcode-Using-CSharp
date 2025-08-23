599fe3706c75111b45fd1ab8c64a91c9d103d4ab
Sarath Uthirasamy <sarath.u1398@gmail.com>
2025-08-24 03:58:39 +0530

    Sliding Window approach for Leetcode #1652

---
+++ SlidingWindow/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Sliding_Window;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var data = new DefuseTheBomb();
+        foreach (var d in data.Decrypt([5, 7, 1, 4], 3))
+        {
+            Console.Write(d + " ");
+        }
+    }
+}
