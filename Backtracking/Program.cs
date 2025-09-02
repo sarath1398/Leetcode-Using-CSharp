7dddd92f65b2ab4db83564f2ba9f9b783496b1f0
Sarath Uthirasamy <sarath.u1398@gmail.com>
2025-09-03 01:12:01 +0530

    Added BinaryWatch solution #LC401

--- Backtracking/Program.cs
+++ Backtracking/Program.cs
@@ -4,15 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        var data = new CombinationSumLC();
-        List<IList<int>> result = new List<IList<int>>(data.CombinationSum([2, 3, 6, 7], 7));
-        foreach (var res in result)
+        // var data = new CombinationSumLC();
+        // List<IList<int>> result = new List<IList<int>>(data.CombinationSum([2, 3, 6, 7], 7));
+        // foreach (var res in result)
+        // {
+        //     foreach (var r in res)
+        //     {
+        //         Console.Write(r + " ");
+        //     }
+        //     Console.WriteLine();
+        // }
+        var data = new BinaryWatch();
+        var result = data.ReadBinaryWatch(1);
+        foreach (var r in result)
         {
-            foreach (var r in res)
-            {
-                Console.Write(r + " ");
-            }
-            Console.WriteLine();
+            Console.Write(r + " ");
         }
     }
 }
