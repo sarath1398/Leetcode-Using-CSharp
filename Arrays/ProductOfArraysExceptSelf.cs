1fa0624b635fb9df528643f41a3e79d9e1a55050
Sarath <sarath.u1398@gmail.com>
2026-01-06 03:08:21 +0530

    Updated Product of Arrays except self program

--- Arrays/ProductOfArraysExceptSelf.cs
+++ Arrays/ProductOfArraysExceptSelf.cs
@@ -5,19 +5,44 @@ namespace Arrays
         public static int[] ProductExceptSelf(int[] nums)
         {
             int n = nums.Length;
-            int sum = 1;
+            //int[] prefixProduct = new int[n];
+            //int[] suffixProduct = new int[n];
             int[] result = new int[n];
+
+            // Build prefix
+            // prefixProduct[0] = nums[0];
+            // for (int i = 1; i < n; i++) {
+            //     prefixProduct[i] = prefixProduct[i - 1] * nums[i];
+            // }
+
+            // Build Prefix
             result[0] = 1;
             for (int i = 1; i < n; i++)
             {
                 result[i] = result[i - 1] * nums[i - 1];
             }
-            for (int j = n - 1; j >= 0; j--)
+
+            // Build suffix
+            // suffixProduct[n - 1] = nums[n - 1];
+            // for (int i = n - 1; i >= 0; i--) {
+            //     suffixProduct[i] = suffixProduct[i + 1] * nums[i];
+            // }
+
+            // Build Suffix
+            int suffix = 1;
+            for (int i = n - 1; i >= 0; i--)
             {
-                result[j] *= sum;
-                sum *= nums[j];
+                result[i] *= suffix;
+                suffix *= nums[i];
             }
 
+
+            // // Build result
+            // for (int i = 0; i < n; i++) {
+            //     int left = (i == 0) ? 1 : prefixProduct[i - 1];
+            //     int right = (i == n - 1) ? 1 : suffixProduct[i + 1];
+            //     result[i] = left * right;
+            // }
             return result;
         }
     }
