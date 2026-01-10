5863ade5bbccd9d0b98a3c35632d625d9ac64711
Sarath <sarath.u1398@gmail.com>
2026-01-10 19:17:18 +0530

    Added Reverse String solution Leetcode 344

---
+++ TwoPointers/Classes.cs
@@ -0,0 +1,50 @@
+﻿namespace TwoPointers
+{
+    public class TwoSum2
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int start = 0;
+            int end = numbers.Length - 1;
+            while (start < end)
+            {
+                int twoSum = numbers[start] + numbers[end];
+                if (twoSum > target)
+                {
+                    end--;
+                    continue;
+                }
+                else if (twoSum < target)
+                {
+                    start++;
+                    continue;
+                }
+                else
+                {
+                    int[] result = { start + 1, end + 1 };
+                    return result;
+                }
+            }
+            return [];
+        }
+    }
+
+    // Approach : Two Pointers
+    // Time Complexity : O(n)
+    // Space Complexity : O(1)
+    // Type: Easy
+    public class ReverseStringLC344
+    {
+        public static void ReverseString(char[] s)
+        {
+            int lptr = 0, rptr = s.Length - 1;
+            while (lptr < rptr)
+            {
+                (s[lptr], s[rptr]) = (s[rptr], s[lptr]);
+                lptr++;
+                rptr--;
+            }
+            Console.WriteLine("[" + string.Join(',',s) + "]");
+        }
+    }
+}
--- TwoPointers/Program.cs
+++ TwoPointers/Program.cs
@@ -4,10 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        var twoSum = new TwoSum2();
-        foreach (var index in twoSum.TwoSum([2,7,11,15],9))
-        {
-            Console.Write(index + " ");            
-        }
+        //var twoSum = new TwoSum2();
+        //foreach (var index in twoSum.TwoSum([2,7,11,15],9))
+        //{
+        //    Console.Write(index + " ");            
+        //}
+
+        ReverseStringLC344.ReverseString(['h', 'e', 'l', 'l', 'o']);
+        ReverseStringLC344.ReverseString(['H', 'a', 'n', 'n', 'a', 'h']);
     }
 }
