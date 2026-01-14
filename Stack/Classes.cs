b1f492a977d874baa7979312bfdddae74f57ac1c
Sarath <sarath.u1398@gmail.com>
2026-01-15 00:27:56 +0530

    Added Baseball game and Valid Parantheses program

---
+++ Stack/Classes.cs
@@ -0,0 +1,75 @@
+﻿namespace Stack
+{
+   public class Classes
+   {
+        public class BaseBallGame
+        {
+            public static int CalPoints(string[] operations)
+            {
+                Stack<int> stack = new();
+                int sum = 0;
+                foreach (string op in operations)
+                {
+                    if (op != "C" && op != "D" && op != "+")
+                    {
+                        int value = int.Parse(op);
+                        sum += value;
+                        stack.Push(value);
+                    }
+                    else if (op == "+")
+                    {
+                        int op1 = stack.Pop();
+                        int op2 = stack.Pop();
+                        int operation = op1 + op2;
+                        sum += operation;
+                        stack.Push(op2);
+                        stack.Push(op1);
+                        stack.Push(operation);
+                    }
+                    else if (op == "C")
+                    {
+                        int value = stack.Pop();
+                        sum -= value;
+                    }
+                    else
+                    {
+                        int op1 = stack.Pop();
+                        int operation = 2 * op1;
+                        sum += operation;
+                        stack.Push(op1);
+                        stack.Push(operation);
+                    }
+                }
+                return sum;
+            }
+        }
+
+        public class ValidParantheses
+        {
+            public static bool IsValid(string s)
+            {
+                Stack<char> stack = new(s.Length);
+                foreach (char c in s)
+                {
+                    if (c == '[' || c == '(' || c == '{')
+                    {
+                        stack.Push(c);
+                    }
+                    else
+                    {
+                        if (stack.Count == 0)
+                            return false;
+
+                        char last = stack.Peek();
+                        if (!(c == ')' && last == '(' || c == '}' && last == '{' || c == ']' && last == '['))
+                        {
+                            return false;
+                        }
+                        stack.Pop();
+                    }
+                }
+                return stack.Count == 0;
+            }
+        }
+   }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿using static Stack.Classes;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(BaseBallGame.CalPoints(["5", "2", "C", "D", "+"]));
+            //Console.WriteLine(BaseBallGame.CalPoints(["5", "-2", "4", "C", "D", "9", "+", "+"]));
+            //Console.WriteLine(BaseBallGame.CalPoints(["1", "C"]));
+
+            Console.WriteLine(ValidParantheses.IsValid("()"));
+            Console.WriteLine(ValidParantheses.IsValid("()[]{}"));
+            Console.WriteLine(ValidParantheses.IsValid("(]"));
+            Console.WriteLine(ValidParantheses.IsValid("([])"));
+            Console.WriteLine(ValidParantheses.IsValid("([)]"));
+        }
+    }
+}
