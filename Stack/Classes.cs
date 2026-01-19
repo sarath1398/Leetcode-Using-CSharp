1ac15377e53466c21f85c700dd1fcfe31913f70e
Sarath <sarath.u1398@gmail.com>
2026-01-19 22:19:30 +0530

    Added Queue using two stacks problem - LC228

--- Stack/Classes.cs
+++ Stack/Classes.cs
@@ -111,5 +111,70 @@
                 }
             }
         }
+
+        public class ImplementQueueUsingTwoStacks
+        {
+            public class MyQueue
+            {
+
+                // TODO : Come up with an amortized O(1) time complexity solution for Peek and Pop operations
+
+                private readonly Stack<int> s1;
+                private readonly Stack<int> s2;
+
+                public MyQueue()
+                {
+                    s1 = new();
+                    s2 = new();
+                }
+
+                public void Push(int x)
+                {
+                    s1.Push(x);
+                }
+
+                public int Pop()
+                {
+                    while (s1.Count > 1)
+                    {
+                        s2.Push(s1.Pop());
+                    }
+                    int value = s1.Pop();
+                    while (s2.Count > 0)
+                    {
+                        s1.Push(s2.Pop());
+                    }
+                    return value;
+                }
+
+                public int Peek()
+                {
+                    while (s1.Count > 1)
+                    {
+                        s2.Push(s1.Pop());
+                    }
+                    int value = s1.Peek();
+                    while (s2.Count > 0)
+                    {
+                        s1.Push(s2.Pop());
+                    }
+                    return value;
+                }
+
+                public bool Empty()
+                {
+                    return s1.Count == 0;
+                }
+            }
+
+            /**
+             * Your MyQueue object will be instantiated and called as such:
+             * MyQueue obj = new MyQueue();
+             * obj.Push(x);
+             * int param_2 = obj.Pop();
+             * int param_3 = obj.Peek();
+             * bool param_4 = obj.Empty();
+             */
+        }
     }
 }
