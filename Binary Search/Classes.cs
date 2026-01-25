e92d1ac98b0dc34d0a7ea91caa794982f93690a5
Sarath <sarath.u1398@gmail.com>
2026-01-26 01:56:42 +0530

    Added Search Insert Position

--- Binary Search/Classes.cs
+++ Binary Search/Classes.cs
@@ -34,5 +34,51 @@
                 return -1;
             }
         }
+
+        // Leetcode : 35 - Search Insert Position
+        // Approach : Binary Search
+        // Time Complexity : O(logn)
+        // Space Complexity : O(1)
+        // Type: Easy
+        public class SearchInsertPosition
+        {
+            public static int SearchInsert(int[] nums, int target)
+            {
+                int n = nums.Length;
+                int end = n - 1;
+                int start = 0;
+                if (target > nums[end])
+                {
+                    return end + 1;
+                }
+                else if (target < nums[start])
+                {
+                    return 0;
+                }
+                while (start <= end)
+                {
+                    int mid = start + (end - start) / 2;
+                    if (nums[mid] == target)
+                    {
+                        return mid;
+                    }
+                    else if (nums[mid] < target && nums[mid + 1] > target)
+                    {
+                        return mid + 1;
+                    }
+                    else if (nums[mid] < target)
+                    {
+                        start = mid + 1;
+                        continue;
+                    }
+                    else
+                    {
+                        end = mid - 1;
+                        continue;
+                    }
+                }
+                return -1;
+            }
+        }
     }
 }
--- Binary Search/Program.cs
+++ Binary Search/Program.cs
@@ -6,8 +6,12 @@ namespace Binary_Search
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(BinarySearch.Search([-1, 0, 3, 5, 9, 12], 9));
-            Console.WriteLine(BinarySearch.Search([-1, 0, 3, 5, 9, 12], 2));
+            // Console.WriteLine(BinarySearch.Search([-1, 0, 3, 5, 9, 12], 9));
+            // Console.WriteLine(BinarySearch.Search([-1, 0, 3, 5, 9, 12], 2));
+
+            Console.WriteLine(SearchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 5));
+            Console.WriteLine(SearchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 2));
+            Console.WriteLine(SearchInsertPosition.SearchInsert(new int[] { 1, 3, 5, 6 }, 7));
         }
     }
 }
