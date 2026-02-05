889ccd26e9ecaa29c73ce4c62eba1ac351858a11
Sarath <sarath.u1398@gmail.com>
2026-02-06 00:03:32 +0530

    Added LinkedList solution - LC239

---
+++ LinkedList/Classes.cs
@@ -0,0 +1,27 @@
+﻿namespace LinkedList
+{
+    internal class Classes
+    {
+        public class ListNode(int val = 0, ListNode? next = null)
+        {
+            public int val = val;
+            public ListNode? next = next;
+        }
+
+        public static ListNode? ReverseList(ListNode? head)
+        {
+            ListNode? prev = null;
+            ListNode? current = head;
+            while (current != null)
+            {
+                ListNode? tempNext = current?.next;
+                current?.next = prev;
+
+                prev = current;
+                current = tempNext;
+            }
+            head = prev;
+            return head;
+        }
+    }
+}
