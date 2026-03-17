1a63d31178ba8b3101ca79b461a81c87d4083754
Sarath <sarath.u1398@gmail.com>
2026-03-18 02:54:15 +0530

    Updated syntax

--- Heap/Program.cs
+++ Heap/Program.cs
@@ -6,7 +6,7 @@ namespace Heap
     {
         static void Main(string[] args)
         {
-            KthLargest kthLargest = new KthLargest(3, new int[] { 4, 5, 8, 2 });
+            KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
             Console.WriteLine(kthLargest.Add(3));   // returns 4
             Console.WriteLine(kthLargest.Add(5));   // returns 5
             Console.WriteLine(kthLargest.Add(10));  // returns 5
